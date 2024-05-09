93d9d370efd2d8661847ef6079f432fb8d5e0823
Rumbler <rumbler@MacBook-Pro-de-Rumbler.local>
2024-05-09 20:56:14 +0200

    My second commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+#Creamos la carpeta con el nombre para guardar todos los archivos
+#Tras eso, creamos el archivo desde VS Code, en la parte izquierda con el icono 'Nuevo archivo..'
+#Añadimos git mediante el terminal y posteriormente, hacemos git add app.py
+#Para añadir un comentario o commit, poner git commit -m "comentario"
+#Cuando el titulo app.py a la izquierda aparece en negro, es porque en working directory y local repository tenemos la misma info
 print("Hello World")
+print("Funcion catastrofica que lo rompe todo")
+#Si queremos ver la difª respecto a la ultima version commiteada, hay que ir a la pestaña 'Control de codigo fuente' (Icono de nodos) a la izq y clicar sobre el nombre de archivo
+print("New Amazing Feat")
+#De nuevo, para guardar, hacemos un  git add
