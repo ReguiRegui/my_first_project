383664dc87181214ae87483287358d82ed2d8169
Rumbler <rumbler@MacBook-Pro-de-Rumbler.local>
2024-05-13 20:05:32 +0200

    new file added

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ print("Functions")
 #Debemos crear otra rama para no modificar los datos en el main/master
 #Para crear la rama, hacemos git branch nombre_de_la_rama
 #Para cambiar de rama, hacemos git checkout nombre_de_la_rama
-#Para fusionar las ramas, hacemos git merge nombre_de_la_rama_a_fusionar
+#Para fusionar las ramas, nos posicionamos en la rama destino y hacemos git merge nombre_de_la_rama_a_fusionar
 #git checkout me muevo yo / git merge muevo los datos
 print("Functins fixed")
 print("Functions: Ble")
